06183123e49afa6280f989c5abce5c416d02fe41
Matheus Almeida e Silva <47538568+MatheusAlmeidaeSilva@users.noreply.github.com>
2023-01-04 16:01:50 -0300

    VPC-MODULO-LOCAL

---
+++ modulo-local/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.73.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "matheusalmeida-remote-state"
+    key    = "aws-vm-modulo-local/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+
+  default_tags {
+    tags = {
+      owner      = "matheusalmeida"
+      managed-by = "terraform"
+    }
+  }
+}
+
+module "network" {
+  source = "./network"
+
+  cidr_vpc    = "10.0.0.0/16"
+  cidr_subnet = "10.0.1.0/24"
+  environment = "desenvolvimento"
+}
